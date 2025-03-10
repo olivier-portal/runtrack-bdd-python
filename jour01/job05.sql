12096775dc4b98b3ed26b080785a7819039a7658
MrRobot <olivier.portal@laplateforme.io>
2025-03-10 10:55:01 +0100

    feat: job05

---
+++ jour01/job05.sql
@@ -0,0 +1,7 @@
+INSERT INTO etudiant (nom, prenom, age, email)
+ VALUES
+ ('Spaghetti', 'Betty', 23, 'betty.Spaghetti@laplateforme.io'),
+ ('Steak', 'Chuck', 45, 'chuck.steak@laplateforme.io'),
+ ('Doe', 'John', 18, 'john.doe@laplateforme.io'),
+ ('Barnes', 'Binkie', 16, 'binkie.barnes@laplateforme.io'),
+ ('Dupuis', 'Gertrude', 20, 'gertrude.dupuis@laplateforme.io');

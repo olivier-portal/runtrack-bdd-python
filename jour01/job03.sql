55442b0c997c429414e894fb758de6db62332d88
MrRobot <olivier.portal@laplateforme.io>
2025-03-10 10:42:47 +0100

    feat: job02 & job03

---
+++ jour01/job03.sql
@@ -0,0 +1,9 @@
+CREATE TABLE etudiant (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(25) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+SHOW TABLES;

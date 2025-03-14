db306542d4a54eb051ff293645e851427ac5aca3
MrRobot <olivier.portal@laplateforme.io>
2025-03-14 10:46:45 +0100

    feat: change to custom tkinter
    feat: create database file

---
+++ jour03_05/database.py
@@ -0,0 +1,38 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Charger les variables d'environnement
+dotenv_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=dotenv_path)
+
+# Récupérer les informations de connexion depuis .env
+USER = os.getenv("USER")
+PASS = os.getenv("PASS")
+
+class Database:
+    def __init__(self):
+        self.db = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user=USER,
+            password=PASS,
+            database="store"
+        )
+        self.cur = self.db.cursor()
+
+    def fetch_categories(self):
+        """Get all categories"""
+        self.cur.execute("SELECT * FROM category")
+        return self.cur.fetchall()
+
+    def fetch_products(self):
+        """Get all products"""
+        self.cur.execute("SELECT * FROM product")
+        return self.cur.fetchall()
+
+    def close(self):
+        """Close db"""
+        self.cur.close()
+        self.db.close()
